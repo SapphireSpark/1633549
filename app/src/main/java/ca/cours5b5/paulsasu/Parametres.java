ef78f36752b4997a4470679eb0b7a53544a13636
SapphireSpark <youZulufi60@gmail.com>
2018-08-28 17:50:23 -0400

    Fait: * Création des messages basés sur la langue choisie * Alternation des message basés sur l'orientation

--- app/src/main/java/ca/cours5b5/paulsasu/Parametres.java
+++ app/src/main/java/ca/cours5b5/paulsasu/Parametres.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Locale;
+
 public class Parametres extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
-        Log.d("MonEtiquette","Bonjour!");
+        Log.d("MonEtiquette",this.getResources().getString(R.string.def));
+
     }
 }
