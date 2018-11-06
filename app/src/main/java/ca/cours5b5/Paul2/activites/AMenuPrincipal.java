cc73b9900f3f12012070db40a4d0e8a8d12d10c0
SapphireSpark <psasu@videotron.ca>
2018-11-06 17:27:09 -0500

    FAIT : * Atelier 11 completé et corrigé.

--- app/src/main/java/ca/cours5b5/Paul2/activites/AMenuPrincipal.java
+++ app/src/main/java/ca/cours5b5/Paul2/activites/AMenuPrincipal.java
@@ -126,6 +126,7 @@ public class AMenuPrincipal extends Activite implements Fournisseur {
             @Override
             public void executer(Object... args) {
                 transitionConnection();
+                
 
             }
         });
